f2d4920fde8a0a1adcf33d008f6cdf5eaf0b71e7
Memo Diaz <121352998+memodiaz44@users.noreply.github.com>
2023-09-27 12:32:20 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Adel Diaz on 27/09/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
